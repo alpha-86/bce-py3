cb1307b303e178c98f78ffa51e91801d6aa2ded4
Chao Chen <alpha.1986@gmail.com>
2019-08-29 10:30:15 +0800

    fix test.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ conf_file_product = 'config_product.yaml'
 if os.path.exists(conf_file_product):
     conf_file = conf_file_product
 f = open(conf_file)
-config = yaml.load(f)
+config = yaml.load(f, Loader=yaml.FullLoader)
